12110a7a145462b5889d9784cdfb81c10675dee4
Qi Zhang <singularitti@outlook.com>
2019-08-26 21:18:37 -0400

    Implement funciton `load_settings`

--- src/Settings.jl
+++ src/Settings.jl
@@ -11,6 +11,23 @@ julia>
 """
 module Settings
 
+import JSON
+import YAML
 
+export load_settings
+
+function load_settings(file::AbstractString)
+    open(file, "r") do io
+        ext = lowercase(splitext(file)[2])
+        dict = if ext == ".json"
+            JSON.parse(io)
+        elseif ext in (".yaml", ".yml")
+            YAML.load(io)
+        else
+            error("Invalid extension!")
+        end
+    end
+    return dict
+end
 
 end
