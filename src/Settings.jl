659f4e3c2e9ad878e664ddbc24566925130abbbf
Qi Zhang <singularitti@outlook.com>
2019-08-26 22:26:34 -0400

    Implement method `parse_settings`
    And fix a bug in `load_settings`

--- src/Settings.jl
+++ src/Settings.jl
@@ -11,10 +11,11 @@ julia>
 """
 module Settings
 
+using EquationsOfState.Collections
 import JSON
 import YAML
 
-export load_settings
+export load_settings, parse_settings
 
 function load_settings(file::AbstractString)
     open(file, "r") do io
@@ -26,8 +27,20 @@ function load_settings(file::AbstractString)
         else
             error("Invalid extension!")
         end
+        return dict
+    end
+end
+
+parse_settings(file::AbstractString) = parse_settings(load_settings(file))
+function parse_settings(d::AbstractDict)
+    dict = deepcopy(d)
+    eos = dict["eos"]
+    eos isa AbstractDict || error("The parameter 'eos' must be a YAML dict!")
+    length(eos) == length(dict["output"]) || throw(DimensionMismatch("The number of outputs is not equal to the number of equations of state given!"))
+    for (key, value) in eos
+        dict["eos"][key] = eval(Symbol(key))(value...)
     end
     return dict
-end
+end  # function parse_settings
 
 end
