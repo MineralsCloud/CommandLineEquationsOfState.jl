2eddf366ae15e9b6a436c9cccbcf336aac3648bd
Qi Zhang <singularitti@outlook.com>
2019-08-27 05:35:33 -0400

    Change "fit" to any command

--- src/CommandLineEquationsOfState.jl
+++ src/CommandLineEquationsOfState.jl
@@ -8,7 +8,8 @@ include("argparse.jl")
 # See [doc](https://docs.julialang.org/en/v1/manual/modules/#Module-initialization-and-precompilation-1)
 function __init__()
     parsed_args = parse_commandline()
-    Settings.run_settings(parsed_args["fit"]["settings"])
+    command = parsed_args["%COMMAND%"]
+    Settings.run_settings(parsed_args[command]["settings"])
 end
 
 end # module
