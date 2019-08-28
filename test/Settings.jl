43b4aa1c7190a2b94fd4c70a99848a8d01f6a4d6
Qi Zhang <singularitti@outlook.com>
2019-08-27 20:49:41 -0400

    test: Add Settings.jl and its data

---
+++ test/Settings.jl
@@ -0,0 +1,11 @@
+#=
+Settings: tests for `CommandLineEquationsOfState.Settings` module
+- Julia version: 1.0
+- Author: singularitti
+- Date: 2019-08-27
+=#
+using Test
+
+@testset "Test 1 dataset with multiple equations of state" begin
+    
+end # testset
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,5 @@ using Test
 
 @testset "CommandLineEquationsOfState.jl" begin
     # Write your own tests here.
+    include("Settings.jl")
 end
